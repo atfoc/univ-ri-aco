ce07b869546dbc7a3f683e4ef5fa5da6fb5e39c2
Vuk Novakovic <vnovakovic96@gmail.com>
2019-01-16 23:57:27 +0100

    small changes in algorithm

--- algorithm/aco.cs
+++ algorithm/aco.cs
@@ -7,86 +7,35 @@ using System.Threading;
 using System.Collections.Generic;
 using System.Collections.Concurrent;
 using System.Linq;
+using System.Diagnostics;
+using graphGenerator;
+using LowerTriangularMatrixNamespace;
+
 
 namespace algorithm
 {
-
+   
     class IterationContext
     {
-        public IterationContext(List<List<int>> antsRoutes, LowerTriangularMatrix pheromoneMatrix
+        public IterationContext(List<List<int>> antsRoutes, LowerTriangularMatrix<double> pheromoneMatrix
                         ,int currIter, int numOfIters){
             this.antsRoutes = antsRoutes;
-            this.pheromoneMatrix = new LowerTriangularMatrix(pheromoneMatrix);
+            this.pheromoneMatrix = new LowerTriangularMatrix<double>(pheromoneMatrix);
             this.currIter = currIter;
             this.numOfIters = numOfIters;
         }
         private List<List<int>> antsRoutes;
-        private LowerTriangularMatrix pheromoneMatrix;
+        private LowerTriangularMatrix<double> pheromoneMatrix;
         private int numOfIters;
         public int currIter{get;private set;}
-    }
-    class LowerTriangularMatrix
-    {
-        public LowerTriangularMatrix(double[,] matrix)
-        {
-            this.size = matrix.GetLength(0);
-            initializeData(matrix);
-        } 
-        public LowerTriangularMatrix(int size)
-        {
-            data = new double[size * (size+1)/2];
-            this.size = size;
-        }
-
-        public LowerTriangularMatrix(LowerTriangularMatrix other){
-            this.size = other.size;
-            data = new double[size * (size+1)/2];
-
-            //deep copy of array
-            for(int i = 0; i < this.size; ++i){
-                this.data[i] = other.data[i];
-            }
-        }
-        double[] data;
-        public int size {get;private set;}
-        private void initializeData(double[,] matrix){
-            int size = matrix.GetLength(0);
-            data = new double[size * (size+1)/2];//LTM has (n*n+1)/2 elements
-            int i = 0, j = 0;
-            for(;i<size;++i){
-                for(j = 0;j<=i;++j){
-                    data[i*(i+1)/2 + j] = matrix[i,j];
-                }
-            }
-        }
-        public double this[int i, int j]
-        {
-            get {
-                if(j > i){
-                    int tmp = i;
-                    i = j;
-                    j = tmp;
-                }
-                return data[i*(i+1)/2 + j];
-            }
-            set {
-                if(j > i){
-                    int tmp = i;
-                    i = j;
-                    j = tmp;
-                }
-                data[i*(i+1)/2 + j] = value;
-            }
-        }
     }
 
     class AntColony
     {
         class Ant
         {
-            //public Ant(){}
-            public Ant(int start, List<int> notVisited, LowerTriangularMatrix pheromoneMatrix,
-                    LowerTriangularMatrix distanceMatrix, double alpha, double beta, bool firstPass) 
+            public Ant(int start, List<int> notVisited, LowerTriangularMatrix<double> pheromoneMatrix,
+                    LowerTriangularMatrix<double> distanceMatrix, double alpha, double beta, bool firstPass) 
             {
                 this.notVisited = notVisited;
                 this.pheromoneMatrix = pheromoneMatrix;
@@ -103,8 +52,8 @@ namespace algorithm
             private int currPosition;
             public List<int> route{get;private set;}
             private List<int> notVisited;
-            LowerTriangularMatrix distanceMatrix;
-            LowerTriangularMatrix pheromoneMatrix;
+            LowerTriangularMatrix<double> distanceMatrix;
+            LowerTriangularMatrix<double> pheromoneMatrix;
             private double alpha, beta;
             public double distanceTraveled{get;private set;}
             private bool firstPass, done;
@@ -141,26 +90,41 @@ namespace algorithm
                 double pheromoneAmount;
                 double distance;
 
-                Dictionary<int, double> attractivness = new Dictionary<int, double>();
-                double sum = 0.0;
-
+                Dictionary<int, decimal> attractivness = new Dictionary<int, decimal>();
+                decimal sum = 0m;
+                int tmpCnt = 0;
                 foreach(int nextPossible in this.notVisited)
                 {
                     distance = distanceMatrix[currPosition, nextPossible];
                     pheromoneAmount = pheromoneMatrix[currPosition, nextPossible];
-
-                    attractivness[nextPossible] =   Math.Pow(pheromoneAmount, alpha)
-                                                    *Math.Pow(1/distance, beta);
+                    decimal pheromonePart = (decimal)Math.Pow(pheromoneAmount, alpha);
+                    //Console.WriteLine("1/" + distance + ":" + Math.Pow(1/distance, beta));
+                    decimal distancePart = (decimal)Math.Pow(1/distance, beta);
+                    attractivness[nextPossible] =   pheromonePart*distancePart;
 
                     sum += attractivness[nextPossible];
+                    tmpCnt++;
                 }
-                //TODO: check if you should check if sum is 0
+                // foreach(var v in attractivness){
+                //     Console.WriteLine(v.Key + "=>" + v.Value);
+                // }
+                
+                
+                if(sum.Equals(0m)){
+                    //since we are changing dict, we need to create a copy of keys
+                    List<int> keys = new List<int>(attractivness.Keys);
+                    foreach(var key in keys){
+                        attractivness[key] = IncrementLastDigit(attractivness[key]);
+                    }
+                    sum = IncrementLastDigit(sum);
+                    //Console.WriteLine($"incemented sum: {sum}");
+                }
 
                 //ROULETE SELECTION
-                double rand = r.NextDouble();
-                double tmpSum = 0;
-                foreach(KeyValuePair<int, double> attr in attractivness){
-                    double weight = attr.Value/sum;
+                decimal rand = (decimal)r.NextDouble();
+                decimal tmpSum = 0;
+                foreach(KeyValuePair<int, decimal> attr in attractivness){
+                    decimal weight = attr.Value/sum;
 
                     if(tmpSum + weight >= rand){
                         return attr.Key;
@@ -170,7 +134,18 @@ namespace algorithm
 
                 return -1;//some error, should not happen
             }
-
+            /*https://stackoverflow.com/questions/2237130/how-to-increase-a-decimals-smallest-fractional-part-by-one */
+            //TODO: fix this function, it has malfunction, always returns 1
+            private decimal IncrementLastDigit(decimal value)
+            {
+                //TODO: check for non 0
+                int[] bits1 = decimal.GetBits(value);
+                int saved = bits1[3];
+                bits1[3] = 0;   // Set scaling to 0, remove sign
+                int[] bits2 = decimal.GetBits(new decimal(bits1) + 1);
+                bits2[3] = saved; // Restore original scaling and sign
+                return new decimal(bits2);
+            }
             private void traverse(int curr, int next)
             {
                 updateRoute(next);
@@ -184,14 +159,14 @@ namespace algorithm
                     double pheromoneEvaporationCoef, double pheromoneConstant, int numOfIters,
                     ConcurrentQueue<IterationContext> cq)
                     {
-                        this.matrix = new LowerTriangularMatrix(matrix);
+                        this.matrix = new LowerTriangularMatrix<double>(matrix);
                         this.start = start;
                         this.antCount = antCount;
                         this.alpha = alpha;
                         this.beta = beta;
 
-                        this.pheromoneMatrix = new LowerTriangularMatrix(matrix.GetLength(0));
-                        this.pheromoneMatrixCopy = new LowerTriangularMatrix(matrix.GetLength(0));
+                        this.pheromoneMatrix = new LowerTriangularMatrix<double>(matrix.GetLength(0));
+                        this.pheromoneMatrixCopy = new LowerTriangularMatrix<double>(matrix.GetLength(0));
                         this.pheromoneEvaporationCoef = pheromoneEvaporationCoef;
                         this.pheromoneConstant = pheromoneConstant;
                         this.numOfIters = numOfIters;
@@ -203,9 +178,32 @@ namespace algorithm
                         this.resultQueue = cq;
                     }
 
-        private LowerTriangularMatrix matrix;
+        public AntColony(  LowerTriangularMatrix<double> matrix, int start, int antCount, double alpha, double beta, 
+                    double pheromoneEvaporationCoef, double pheromoneConstant, int numOfIters,
+                    ConcurrentQueue<IterationContext> cq)
+                    {
+                        this.matrix = new LowerTriangularMatrix<double>(matrix);
+                        this.start = start;
+                        this.antCount = antCount;
+                        this.alpha = alpha;
+                        this.beta = beta;
+
+                        this.pheromoneMatrix = new LowerTriangularMatrix<double>(matrix.size);
+                        this.pheromoneMatrixCopy = new LowerTriangularMatrix<double>(matrix.size);
+                        this.pheromoneEvaporationCoef = pheromoneEvaporationCoef;
+                        this.pheromoneConstant = pheromoneConstant;
+                        this.numOfIters = numOfIters;
+                        ants = new Ant[antCount];
+                        this.firstPass = true;
+                        this.shortestPath = null;
+                        this.shrotestDistance = -1;
+                        initAnts();
+                        this.resultQueue = cq;
+                    }
+
+        private LowerTriangularMatrix<double> matrix;
         private Ant[] ants;
-        LowerTriangularMatrix pheromoneMatrix, pheromoneMatrixCopy;
+        LowerTriangularMatrix<double> pheromoneMatrix, pheromoneMatrixCopy;
         private int start, antCount, numOfIters;
         private double alpha, beta, pheromoneEvaporationCoef, pheromoneConstant;
         private bool firstPass;
@@ -277,11 +275,11 @@ namespace algorithm
                 updatePheromoneMatrix();
                 this.firstPass = false;
                 initAnts();
-                this.pheromoneMatrixCopy = new LowerTriangularMatrix(matrix.size);
+                this.pheromoneMatrixCopy = new LowerTriangularMatrix<double>(matrix.size);
                 this.resultQueue.Enqueue(new IterationContext(antsRoutes,pheromoneMatrix,currIter,
                                                             numOfIters));
                 ++currIter;
-                System.Threading.Thread.Sleep(100);//for testing purposes TODO: remove
+                //System.Threading.Thread.Sleep(100);//for testing purposes TODO: remove
                 
             }
         }
@@ -292,45 +290,61 @@ namespace algorithm
         static void Main(string[] args)
         {
             //testing matrix
-            double[,] matrix = new double[6,6]{ {-1,7,20,7,6,8},
-                                                {7,-1,11,8,13,11},
-                                                {20,11,-1,18,19,7},
-                                                {7,8,18,-1,3,2},
-                                                {6,13,19,3,-1,5},
-                                                {8,11,7,2,5,-1}};
+            // double[,] matrix = new double[6,6]{ {-1,7,20,7,6,8},
+            //                                     {7,-1,11,8,13,11},
+            //                                     {20,11,-1,18,19,7},
+            //                                     {7,8,18,-1,3,2},
+            //                                     {6,13,19,3,-1,5},
+            //                                     {8,11,7,2,5,-1}};
             ConcurrentQueue<IterationContext> cq = new ConcurrentQueue<IterationContext>();
 
             int start = 0;
             int antCount = 50;
             double alpha = .5;
-            double beta  = 1.2;
+            double beta  = 0.8;
             double pheromoneEvaporationCoef = .40;
-            double pheromoneConstant = 1000;
-            int maxIters = 80;
+            double pheromoneConstant = 500;
+            int maxIters = 100;
 
-        
-            AntColony ac = new AntColony(matrix,start,antCount,alpha,beta,pheromoneEvaporationCoef,
-                                        pheromoneConstant ,maxIters, cq);
+            int numOfGraphs = 1;
+            int maxNodes = 45;
+            int minNodes = 10;
+            int maxWeight = 3000;
+            int minWeight = 500;
+            Generator g = new Generator(numOfGraphs, maxNodes, minNodes, maxWeight, minWeight);
 
-            Thread t = new Thread(new ThreadStart(ac.mainLoop));
-            t.Start();
-            IterationContext currIterContext;
-            int currIter = 0;
-            while(currIter < maxIters - 1){
-                while(cq.TryDequeue(out currIterContext) == false){
-                    //Console.WriteLine("Waiting");
-                }
-                Console.WriteLine($"New iter: {currIterContext.currIter}");
-                currIter = currIterContext.currIter;
+            var graphs = g.generate(); //array of matricies of graphs
+            Console.WriteLine("Graphs generated");
+            foreach(LowerTriangularMatrix<double> lt in graphs)
+            {
+                lt.writeToFile("test.txt");
+                Console.WriteLine(lt.size);
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                AntColony ac = new AntColony(lt,start,antCount,alpha,beta,pheromoneEvaporationCoef,
+                                            pheromoneConstant ,maxIters, cq);
+
+                Thread t = new Thread(new ThreadStart(ac.mainLoop));
+                t.Start();
+                // IterationContext currIterContext;
+                // int currIter = 0;
+                // while(currIter < maxIters - 1){
+                //     while(cq.TryDequeue(out currIterContext) == false){
+                //         //Console.WriteLine("Waiting");
+                //     }
+                //     Console.WriteLine($"New iter: {currIterContext.currIter}");
+                //     currIter = currIterContext.currIter;
+                // }
+                t.Join();
+                sw.Stop();
+
+                var path = string.Join("=>",ac.shortestPath);
+                var dist = ac.shrotestDistance;
+
+                Console.WriteLine($"Elapsed time: {sw.Elapsed}");
+                Console.WriteLine($"Shortest path has length of {dist} and it is: {path}");
+
             }
-            t.Join();
-
-            var path = string.Join("=>",ac.shortestPath);
-            var dist = ac.shrotestDistance;
-        
-
-            Console.WriteLine($"Shortest path has length of {dist} and it is: {path}");
-            
 
             
             // foreach(int node in path){
